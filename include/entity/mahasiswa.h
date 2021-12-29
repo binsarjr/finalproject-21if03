e08803626c667592ed0a33b9c10acc2935c23911
Binsar Dwi Jasuma <binsarjr121@gmail.com>
2021-12-29 09:59:46 +0700

    entity

---
+++ include/entity/mahasiswa.h
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+struct Mahasiswa
+{
+    string npm, nama;
+};
---
+++ include/entity/mataKuliah.h
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std;
+
+struct MataKuliah
+{
+    string nama, kodeMatkul;
+};
